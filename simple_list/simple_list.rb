9376d8fb29539da8e217606c55eb7b6e88ed54f6
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-08-08 18:44:55 +0200

    Raw recursive simple_list first steps Funcionalidades que se han tratado en el commit: 	-Creacion de nodos 	-Busqueda de un Nodo Probablemente no funciona, por problemas con el MiniTest no he podido correrlos. Es una version en crudo casi en pseudo

---
+++ simple_list/simple_list.rb
@@ -0,0 +1,78 @@
+require "minitest/autorun"
+
+class TestSimpleList < Minitest::Test
+
+  class Node 
+    def initialize(value)
+      @value = value
+      @next_node = nil
+      @previous_node = nil
+    end
+
+     def initialize(value, previous_node)
+      @value = value
+      @next_node = nil
+      @previous_node = :previous_node
+    end
+
+  end
+
+  class List
+
+    @node = nil
+
+    def find(value)
+      point_first
+      find_from_beginning(@node, value)
+      @node.value == value ? @node : nil
+    end
+
+    def find_from_beginning(value)
+      @node.value != value && @node.next_node != nil  ? find(@node.next_node, value) : nilß
+    end
+
+    def point_first
+      @node.previous_node == nil ? @node : point_first(@node.previous_node)
+    end
+
+    def point_last
+      @node.next_node == nil ? @node : point_last(@node.next_node)
+    end
+
+    def add(value)
+      firt_node? ? @node = Node.new(value) : add_node_to_list(value)     
+    end
+
+    def add_node_to_list(value)
+      point_last
+      @node.next_node = Node.new(value, @node)
+      @node = @node.next_node
+    end
+
+    def firt_node?(node)
+      point_first
+      node == @node ? true : false
+    end
+
+  end
+
+# EMPEZAMOS LOS TEST
+
+  def create_a_list
+    @the_list = List.new
+    @the_list.add('Primero')
+    @the_list.add('Segundo')
+    @the_list.add('Tercero')
+    @the_list.add('Cuarto')
+  end
+
+
+
+  def find_recursive
+    assert_equal('Tercero',find('Tercero'))
+    assert_equal(nil ,find('Quinto'))
+  end
+
+  
+
+end
