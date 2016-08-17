9b19ce5ed31fbc5a2625466f4ad6828688b66a81
Sergio <immawinnerbaby92@gmail.com>
2016-08-17 18:41:02 +0200

    generate basket tested

--- Potter_2.0/Potter.rb
+++ Potter_2.0/Potter.rb
@@ -6,14 +6,23 @@ class TestPotter < Minitest::Test
 
   	@groups_by_book = Hash.new
 	@basket = Array.new
+	@BOOK_PRICE = 8
+    @DISCOUNTS = {
+      1 => 1,
+      2 => 0.95,
+      3 => 0.9,
+      4 => 0.85,
+      5 => 0.8,
+      6 => 0.7,
+      7 => 0.55
+    }
+    attr_accessor :groups_by_book, :basket
 
     def self.price(books)
-      
       group_books_by_id(books)
-      
-      generate_basket(@groups_by_book)
+      generate_basket
       final_price = apply_discounts(@basket)
-      price
+      final_price
     end
 
     def self.group_books_by_id(books)
@@ -27,18 +36,35 @@ class TestPotter < Minitest::Test
       @groups_by_book
     end
 
+    def self.generate_basket
+      while !@groups_by_book.empty?
+      	number_of_books_in_collection = 0
+      	@groups_by_book.each do |key, value|
+      	  if @groups_by_book[key] == 1 then
+      	  	number_of_books_in_collection += 1
+      	  	@groups_by_book.delete(key)
+      	  else
+      	  	number_of_books_in_collection += 1
+      	  	@groups_by_book[key] -= 1
+      	  end
+      	end
+      	@basket.push(number_of_books_in_collection)
+      end
+      @basket
+    end
+
   end 
 
   describe Potter do
 
-    it "Buy 1 book and this one cost 8$" do
+    it "Gave an bunch of books and get them grouped by id" do
       #assert_equal({2 => 1}, Potter.group_books_by_id([2]))
       assert_equal({1 => 1 ,2 => 3}, Potter.group_books_by_id([2,2,2,1]))
       #assert_equal({3 => 1}, Potter.group_books_by_id([3]))
     end
 
-    it "Buy 1 book and this one cost #{8 * 2 * 0.95}" do
-      assert_equal(true, true)
+    it "Gave the books grouped and get the collections, ready to apply the discount" do
+      assert_equal([2,1,1],Potter.generate_basket)
     end
 
   end
