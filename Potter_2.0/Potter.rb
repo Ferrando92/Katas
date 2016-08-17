d38fe45018e049803a37826b4d58d8621f5a1265
Sergio <immawinnerbaby92@gmail.com>
2016-08-17 18:53:09 +0200

    solved Sublime issue

--- Potter_2.0/Potter.rb
+++ Potter_2.0/Potter.rb
@@ -3,10 +3,9 @@ require "minitest/autorun"
 class TestPotter < Minitest::Test
 
   class Potter 
-
-  	@groups_by_book = Hash.new
-	@basket = Array.new
-	@BOOK_PRICE = 8
+    @groups_by_book = Hash.new
+    @basket = Array.new
+    @BOOK_PRICE = 8
     @DISCOUNTS = {
       1 => 1,
       2 => 0.95,
@@ -26,11 +25,11 @@ class TestPotter < Minitest::Test
     end
 
     def self.group_books_by_id(books)
-   	  books.each do |book_id|
-	    if !@groups_by_book[book_id] then
-	      @groups_by_book[book_id] = 1 
-	    else 
-	   	  @groups_by_book[book_id] = @groups_by_book[book_id]+1	
+      books.each do |book_id|
+        if !@groups_by_book[book_id] then
+          @groups_by_book[book_id] = 1 
+        else 
+   	      @groups_by_book[book_id] = @groups_by_book[book_id]+1	
         end
       end
       @groups_by_book
@@ -38,21 +37,20 @@ class TestPotter < Minitest::Test
 
     def self.generate_basket
       while !@groups_by_book.empty?
-      	number_of_books_in_collection = 0
-      	@groups_by_book.each do |key, value|
-      	  if @groups_by_book[key] == 1 then
-      	  	number_of_books_in_collection += 1
-      	  	@groups_by_book.delete(key)
-      	  else
-      	  	number_of_books_in_collection += 1
-      	  	@groups_by_book[key] -= 1
-      	  end
-      	end
-      	@basket.push(number_of_books_in_collection)
+  	    number_of_books_in_collection = 0
+  	    @groups_by_book.each do |key, value|
+  	      if @groups_by_book[key] == 1 then
+  	        number_of_books_in_collection += 1
+  	  	    @groups_by_book.delete(key)
+  	      else
+  	  	    number_of_books_in_collection += 1
+  	  	    @groups_by_book[key] -= 1
+  	      end
+  	    end
+  	    @basket.push(number_of_books_in_collection)
       end
       @basket
     end
-
   end 
 
   describe Potter do
@@ -66,59 +64,57 @@ class TestPotter < Minitest::Test
     it "Gave the books grouped and get the collections, ready to apply the discount" do
       assert_equal([2,1,1],Potter.generate_basket)
     end
-
   end
-
+end
 =begin  #DEPRECATED
-    class Test
+class Test
 
-    def self.price (arry)
-      @BOOK_PRICE = 8
-      @DISCOUNTS = {
-        1 => 1,
-        2 => 0.95,
-        3 => 0.9,
-        4 => 0.85,
-        5 => 0.8,
-        6 => 0.7,
-        7 => 0.55
-      }
-      @basket = create_basket(arry)
-      puts '----------'
-      puts @basket
-      puts '----------'
-      apply_discounts()
-    end
+def self.price (arry)
+  @BOOK_PRICE = 8
+  @DISCOUNTS = {
+    1 => 1,
+    2 => 0.95,
+    3 => 0.9,
+    4 => 0.85,
+    5 => 0.8,
+    6 => 0.7,
+    7 => 0.55
+  }
+  @basket = create_basket(arry)
+  puts '----------'
+  puts @basket
+  puts '----------'
+  apply_discounts()
+end
 
-    def self.create_basket(books)
-      basket = []
-      books.each do |book|
-        basket[book] == nil ? basket[book] = 1 : basket[book] = basket[book]+1
-      end
-    end
-
-    def self.create_collections()
-      @collection = []
-      while @basket.size > 0
-        iterator = @basket.size
-        @basket.each.with_index do |book, i|
-          @basket[i] = @basket[i]-1
-         if @basket[i] <= 0 then
-           @basket.delete_at(i)         	
-         end
-        end
-        @collection.push(iterator)
-      end
-    end
-
-    def self.apply_discounts ()
-      @budget = 0
-      create_collections
-      @collection
-    end
-    
+def self.create_basket(books)
+  basket = []
+  books.each do |book|
+    basket[book] == nil ? basket[book] = 1 : basket[book] = basket[book]+1
   end
-	
-=end
+end
+
+def self.create_collections()
+  @collection = []
+  while @basket.size > 0
+    iterator = @basket.size
+    @basket.each.with_index do |book, i|
+      @basket[i] = @basket[i]-1
+     if @basket[i] <= 0 then
+       @basket.delete_at(i)         	
+     end
+    end
+    @collection.push(iterator)
+  end
+end
+
+def self.apply_discounts ()
+  @budget = 0
+  create_collections
+  @collection
+end
 
 end
+
+=end
+
