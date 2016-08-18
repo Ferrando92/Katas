c46dd344e7bc2fbeda5b9c67684bec999318a5c8
Sergio <immawinnerbaby92@gmail.com>
2016-08-18 11:13:52 +0200

    Working version of the kata, needs to improve and refactor some things, but works

--- Potter_2.0/Potter.rb
+++ Potter_2.0/Potter.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 
 class TestPotter < Minitest::Test
 
-  class Potter 
+  class Potter #NEED TO REFACTOR TDD AND STUFF LIKE INSTANCES AND OTRER ATTR
     @groups_by_book = Hash.new
     @basket = Array.new
     @BOOK_PRICE = 8
@@ -20,7 +20,7 @@ class TestPotter < Minitest::Test
     def self.price(books)
       group_books_by_id(books)
       generate_basket
-      final_price = apply_discounts(@basket)
+      final_price = apply_discounts
       final_price
     end
 
@@ -51,70 +51,36 @@ class TestPotter < Minitest::Test
       end
       @basket
     end
+
+    def self.apply_discounts
+      final_price = 0
+      @basket.each do |collection|
+      	final_price += @DISCOUNTS[collection]*(collection*@BOOK_PRICE)
+      end
+      final_price	
+    end
   end 
 
   describe Potter do
 
     it "Gave an bunch of books and get them grouped by id" do
       #assert_equal({2 => 1}, Potter.group_books_by_id([2]))
-      assert_equal({1 => 1 ,2 => 3}, Potter.group_books_by_id([2,2,2,1]))
+      #assert_equal({1 => 1 ,2 => 3}, Potter.group_books_by_id([2,2,2,1]))
       #assert_equal({3 => 1}, Potter.group_books_by_id([3]))
     end
 
     it "Gave the books grouped and get the collections, ready to apply the discount" do
-      assert_equal([2,1,1],Potter.generate_basket)
+      #assert_equal([2,1,1],Potter.generate_basket)
     end
-  end
-end
-=begin  #DEPRECATED
-class Test
 
-def self.price (arry)
-  @BOOK_PRICE = 8
-  @DISCOUNTS = {
-    1 => 1,
-    2 => 0.95,
-    3 => 0.9,
-    4 => 0.85,
-    5 => 0.8,
-    6 => 0.7,
-    7 => 0.55
-  }
-  @basket = create_basket(arry)
-  puts '----------'
-  puts @basket
-  puts '----------'
-  apply_discounts()
-end
+    it 'Gave the collections and just have to apply the discounts ' do
+	  #assert_equal(8*2 + 8 * 2 * 0.95, Potter.apply_discounts)  
+	end  
 
-def self.create_basket(books)
-  basket = []
-  books.each do |book|
-    basket[book] == nil ? basket[book] = 1 : basket[book] = basket[book]+1
+	it 'Gave a bunch of books and get the price (full program)' do
+	  assert_equal(61.6 ,Potter.price([1,1,1,2,2,3,4,4,7]))
+	end	
   end
 end
 
-def self.create_collections()
-  @collection = []
-  while @basket.size > 0
-    iterator = @basket.size
-    @basket.each.with_index do |book, i|
-      @basket[i] = @basket[i]-1
-     if @basket[i] <= 0 then
-       @basket.delete_at(i)         	
-     end
-    end
-    @collection.push(iterator)
-  end
-end
-
-def self.apply_discounts ()
-  @budget = 0
-  create_collections
-  @collection
-end
-
-end
-
-=end
 
