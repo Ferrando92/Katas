a67c1a7818224566c38768e3837bb9e27dd069e0
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-10-10 15:27:35 +0200

    simple moves

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -1,6 +1,34 @@
 class MarsRover
-  INNITIAL_POSITION = [0,0]
+  def land(plannet, position, orientation)
+    @plannet = plannet
+    @actual_position_x = position[0]
+    @actual_position_y = position[1]
+    @actual_orientation = orientation
+  end
+
   def broadcast_position
-    INNITIAL_POSITION
+    obtain_actual_position
+  end
+
+  def obtain_actual_position
+    [@actual_position_x, @actual_position_y]
+  end
+
+  def broadcast_orientation
+    @actual_orientation
+  end
+
+  def receive_command(command)
+    move_forward if command == :F
+    move_backward if command == :B
+  end
+
+  def move_forward
+    @actual_position_y += 1
+  end
+
+  def move_backward
+    @actual_position_y -= 1
+    @actual_position_y = @plannet[@actual_position_x].length - 1
   end
 end
--- mars_rover/spec/mars_rover_spec.rb
+++ mars_rover/spec/mars_rover_spec.rb
@@ -1,12 +1,50 @@
 require './mars_rover'
-describe MarsRover, '#first movements'  do
+describe MarsRover, '#first movements' do
+  INNITIAL_POSITION = [0, 0]
+  INNITIAL_ORIENTATION = [:E]
+  LAST_EAST_POSITION_Y = 49
+  LAST_WEST_POSITION_Y = 0
+  LAST_SOUTH_POSITION_X = 49
+  LAST_NORTH_POSITION_X = 0
+
+  before :each do
+    @mars_rover = MarsRover.new
+    @plannet = Array.new(LAST_SOUTH_POSITION_X + 1, Array.new(LAST_EAST_POSITION_Y + 1))
+    @mars_rover.land(@plannet, INNITIAL_POSITION, INNITIAL_ORIENTATION)
+  end
+
   context 'The rover just landed' do
-    it 'the rover is in the position [0,0]' do
-      INNITIAL_POSITION = [0,0]
-      INNITIAL_ORIENTATION = [:north]
-      mars_rover = MarsRover.new(INNITIAL_POSITION, INNITIAL_ORIENTATION)
-      expect(mars_rover.broadcast_position).to eq(INNITIAL_POSITION)
-      expect(mars_rover.broadcast_orientation).to eq(INNITIAL_ORIENTATION)
+    it 'the rover is in the position [0, 0]' do
+      expect(@mars_rover.broadcast_position).to eq(INNITIAL_POSITION)
+    end
+    it 'the rover is oriented to East' do
+      expect(@mars_rover.broadcast_orientation).to eq(INNITIAL_ORIENTATION)
+    end
+  end
+
+  context 'Trying to make some simple moves sending commands' do
+    it 'the rover moves forward' do
+      @mars_rover.receive_command(:F)
+      expect(@mars_rover.broadcast_position).to eq([0, 1])
+    end
+    it 'the rover moves backward' do
+      @mars_rover.receive_command(:B)
+      expect(@mars_rover.broadcast_position).to eq([0, LAST_EAST_POSITION_Y])
+    end
+  end
+
+  context 'Trying to make some more tricky moves sending commands' do
+    it 'the rover moves up' do
+      # expect(@mars_rover.broadcast_position).to eq()
+    end
+    it 'the rover moves down' do
+      # expect(@mars_rover.broadcast_orientation).to eq(INNITIAL_ORIENTATION)
+    end
+    it 'the rover moves left' do
+      # expect(@mars_rover.broadcast_orientation).to eq(INNITIAL_ORIENTATION)
+    end
+    it 'the rover moves right' do
+      # expect(@mars_rover.broadcast_orientation).to eq(INNITIAL_ORIENTATION)
     end
   end
 end
