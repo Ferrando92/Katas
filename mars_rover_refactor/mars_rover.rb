0c0cc3058c82053f059e237baf0c7e80c59b72cb
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-10-18 18:26:09 +0200

    rubocopped

--- mars_rover_refactor/mars_rover.rb
+++ mars_rover_refactor/mars_rover.rb
@@ -1,9 +1,9 @@
 class MarsRover
   X_POSITION = 0
-  Y_POSITION =  1
-  TURNS_RIGHT_MAPPING = {:NORTH => :EAST , :EAST=> :SOUTH, :SOUTH=> :WEST, :WEST=> :NORTH}
-  TURNS_LEFT_MAPPING = {:NORTH => :WEST , :EAST=> :NORTH, :SOUTH=> :EAST, :WEST=> :SOUTH}
-  STEP_FORWARD_MAPPING = {:NORTH => X_POSITION , :EAST=> Y_POSITION, :SOUTH=> X_POSITION, :WEST=> Y_POSITION}
+  Y_POSITION = 1
+  TURNS_RIGHT_MAPPING = { NORTH: :EAST , EAST: :SOUTH, SOUTH: :WEST, WEST: :NORTH }
+  TURNS_LEFT_MAPPING = { NORTH: :WEST , EAST: :NORTH, SOUTH: :EAST, WEST: :SOUTH }
+  STEP_FORWARD_MAPPING = { NORTH: X_POSITION, EAST: Y_POSITION, SOUTH: X_POSITION, WEST: Y_POSITION }
   RIGHT = TURNS_RIGHT_MAPPING
   LEFT = TURNS_LEFT_MAPPING
   def land(plannet, position, orientation)
@@ -30,7 +30,7 @@ class MarsRover
     @actual_orientation
   end
 
-  def command(command_message)
+  def recieve_command(command_message)
     move_forward if command_message == :F
     move_backward if command_message == :B
     turn RIGHT if command_message == :R
@@ -40,8 +40,8 @@ class MarsRover
   end
 
   def move_forward
-     take_one_step_forward if !is_at_the_edge?
-     do_a_barrel_roll if is_at_the_edge?
+    take_one_step_forward unless at_the_edge?
+    do_a_barrel_roll if at_the_edge?
   end
 
   def move_backward
@@ -50,23 +50,23 @@ class MarsRover
     move_forward
   end
 
-  def is_at_the_edge?# switch?
+  def at_the_edge?# switch?
     if @actual_orientation == :EAST
       @actual_position_y == @plannet[@actual_position_x].length - 1
-      #@actual_position[Y_POSITION] == @plannet[@actual_position[X_POSITION]].length - 1
+      # @actual_position[Y_POSITION] == @plannet[@actual_position[X_POSITION]].length - 1
     elsif @actual_orientation == :WEST
-      @actual_position_y == 0
+      @actual_position_y.zero?
       #  @actual_position[Y_POSITION] == 0
     elsif @actual_orientation == :SOUTH
       @actual_position_x == @plannet.length - 1
       # @actual_position[X_POSITION]== @plannet.length - 1
     elsif @actual_orientation == :NORTH
-      @actual_position_x == 0
+      @actual_position_x.zero?
       # @actual_position[X_POSITION] == 0
     end
   end
 
-def take_one_step_forward
+  def take_one_step_forward
     if @actual_orientation == :NORTH
       @actual_position_x += 1
     elsif @actual_orientation == :SOUTH
@@ -78,14 +78,12 @@ def take_one_step_forward
     end
   end
 
-
   def take_one_step_forward_refactored
      @actual_position[STEP_FORWARD_MAPPING[@actual_orientation]] += 1
   end
 
-  def turn side
+  def turn(side)
     @actual_orientation = side[@actual_orientation]
-
   end
   def do_a_barrel_roll # Totally random name, SWITCH?
     if @actual_orientation == :EAST
@@ -99,4 +97,3 @@ def take_one_step_forward
     end
   end
 end
-
--- mars_rover_refactor/spec/mars_rover_spec.rb
+++ mars_rover_refactor/spec/mars_rover_spec.rb
@@ -24,43 +24,43 @@ describe MarsRover, '#first movements' do
 
   context 'Trying to make some simple moves sending commands' do
     it 'the rover moves forward' do
-      @mars_rover.command(:F)
+      @mars_rover.recieve_command(:F)
       expect(@mars_rover.broadcast_position).to eq([0, 1])
     end
     it 'the rover moves backward' do
-      @mars_rover.command(:B)
+      @mars_rover.recieve_command(:B)
       expect(@mars_rover.broadcast_position).to eq([0, LAST_EAST_POSITION_Y])
     end
     it 'the rover turns left' do
-      @mars_rover.command(:L)
+      @mars_rover.recieve_command(:L)
       expect(@mars_rover.broadcast_orientation).to eq(:NORTH)
     end
     it 'the rover turns right' do
-      @mars_rover.command(:R)
+      @mars_rover.recieve_command(:R)
       expect(@mars_rover.broadcast_orientation).to eq(:SOUTH)
     end
   end
 
   context 'Trying to make some more tricky moves sending commands, using the table like a reference not the rover' do
     it 'the rover moves up' do
-      @mars_rover.command(:L)
-      @mars_rover.command(:F)
-      expect(@mars_rover.broadcast_position).to eq([49,0])
+      @mars_rover.recieve_command(:L)
+      @mars_rover.recieve_command(:F)
+      expect(@mars_rover.broadcast_position).to eq([49, 0])
     end
     it 'the rover moves down' do
-      @mars_rover.command(:R)
-      @mars_rover.command(:F)
-      expect(@mars_rover.broadcast_position).to eq([1,0])
+      @mars_rover.recieve_command(:R)
+      @mars_rover.recieve_command(:F)
+      expect(@mars_rover.broadcast_position).to eq([1, 0])
     end
     it 'the rover moves to the left' do
-      @mars_rover.command(:L)
-      @mars_rover.command(:L)
-      @mars_rover.command(:F)
-      expect(@mars_rover.broadcast_position).to eq([0,49])
+      @mars_rover.recieve_command(:L)
+      @mars_rover.recieve_command(:L)
+      @mars_rover.recieve_command(:F)
+      expect(@mars_rover.broadcast_position).to eq([0, 49])
     end
     it 'the rover moves to the right' do
-      @mars_rover.command(:F)
-      expect(@mars_rover.broadcast_position).to eq([0,1])
+      @mars_rover.recieve_command(:F)
+      expect(@mars_rover.broadcast_position).to eq([0, 1])
     end
   end
 end
